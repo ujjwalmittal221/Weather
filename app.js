cc31f8ab8dfb6f80f2772474e5922c26d7a95300
Ujjwal Mittal <ujjwalmittal221@gmail.com>
2023-05-15 10:03:11 +0530

    Fourth Commit

--- app.js
+++ app.js
@@ -28,10 +28,11 @@ app.post("/",function(req,res){
             const icon = weatherData.weather[0].icon;
             const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-           res.write("<p>The weather is currently " + weatherDescription + ".</p>");
-           res.write("<h1>The temperature in " + query + " is " + temp + " degrees celsius.</h1>");
-           res.write("<img src =" + imageURL +">");
-           res.send();
+            res.setHeader("Content-Type", "text/html");
+            res.write("<p>The weather is currently " + weatherDescription + ".</p>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees celsius.</h1>");
+            res.write("<img src =" + imageURL +">");
+            res.send();
         });
     });
 });
